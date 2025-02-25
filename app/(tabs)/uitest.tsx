45cdf259be6669315b7edd953397570c8052cabb
Typhaine <typhaine.legales@upian.com>
2025-02-25 12:19:36 +0100

    feat: working controls

--- app/(tabs)/uitest.tsx
+++ app/(tabs)/uitest.tsx
@@ -1,4 +1,5 @@
 import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
 import {  useRef } from "react";
 import { StyleSheet, Text } from 'react-native';
 import Rive, {Fit, RiveRef} from 'rive-react-native';
@@ -8,14 +9,15 @@ export default function CustomRive() {
         <ThemedView style={styles.container}>
           <Rive
           ref={riveComponentRef}
-          resourceName="screw"
+          resourceName="screwed3"
           fit={Fit.Cover}
           style={{
-            width: "100%",
-            height: "100%",
+  
+            width:"100%"
           }}
           stateMachineName="screw"
         />
+        <ThemedText type="title">Let's screw</ThemedText>
       </ThemedView>
     )
 }
