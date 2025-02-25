c77d1163dfb8bd84931b379f4e6c42bfd4b55381
Typhaine Le Gales <typhaineLeGales@protonmail.com>
2025-02-25 09:19:19 +0100

    feat: local file

--- app/(tabs)/uitest.tsx
+++ app/(tabs)/uitest.tsx
@@ -1,10 +1,22 @@
 import { ThemedView } from '@/components/ThemedView';
+import {  useRef } from "react";
 import { StyleSheet, Text } from 'react-native';
+import Rive, {Fit, RiveRef} from 'rive-react-native';
 export default function CustomRive() {
+  const riveComponentRef = useRef<RiveRef>(null);
     return (
         <ThemedView style={styles.container}>
-            <Text>Test custom animation</Text>
-        </ThemedView>
+          <Rive
+          ref={riveComponentRef}
+          resourceName="screw"
+          fit={Fit.Cover}
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+          stateMachineName="screw"
+        />
+      </ThemedView>
     )
 }
 const styles = StyleSheet.create({
